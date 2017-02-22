d6014cf6c0641985b3859b46812a915c379b0e87
CarbonJ <justin.hanssen@gmail.com>
2017-02-22 18:53:38 -0500

    Cleanup prior to slack
    Added information before making Slack attempt.

--- genconhotels.py
+++ genconhotels.py
@@ -42,6 +42,7 @@ class Scraper(object):
 
         # Input calendar parameters
         try:
+            # DEFECT: Not getting proper values submitted via selenium
             driver.find_element_by_css_selector("h5.accordion").click()
             driver.find_element_by_link_text("16").click()
             driver.find_element_by_css_selector(
@@ -95,7 +96,6 @@ class Scraper(object):
                             addy.get_text().strip().replace(u'\xa0', u' '))
                         templist.append('.'.join(temp_price))
                         templist.append('.'.join(temp_mi))
-                        #logger.info(templist)
                         self.listings.append(templist)
 
                 with open("results.html", "w") as temp:
@@ -123,82 +123,6 @@ class Scraper(object):
 if __name__ == "__main__":
     logger.info("Start of scraper.")
     scraper = Scraper(url)
-    scraper.scrap(1, 3)
+    scraper.scrap(1, 3)  # # of rooms, # of guests
     logger.info(scraper.listings)
     logger.info("End of scraper.")
-
-# # Needed variables
-# url = "https://aws.passkey.com/reg/32X3LVML-G0EF/"
-
-# # Selenium getting page
-# driver = webdriver.PhantomJS()
-# driver.set_window_size(1120, 550)
-
-# driver.get(url)
-
-# try:
-#     driver.find_element_by_css_selector("h5.accordion").click()
-#     driver.find_element_by_link_text("16").click()
-#     driver.find_element_by_css_selector("#check-out > h5.accordion").click()
-#     driver.find_element_by_xpath("(//a[contains(text(),'20')])[2]").click()
-# except Exception as e:
-#     raise e
-#     print(e)
-
-# try:
-#     driver.find_element_by_id("spinner-room").clear()
-#     driver.find_element_by_id("spinner-room").click()
-#     driver.find_element_by_id("spinner-room").send_keys("2")
-
-#     driver.find_element_by_id("spinner-guest").clear()
-#     driver.find_element_by_id("spinner-guest").send_keys("4")
-# except Exception as e:
-#     raise e
-#     print(e)
-
-# try:
-#     driver.find_element_by_link_text("FIND").click()
-#     delay = 5  # seconds
-# except driver.NoSuchElementException as e:
-#     driver.save_screenshot('screenshot.png')
-#     raise e
-#     print(e)
-
-# html = driver.page_source
-
-# if "Please select one" in html:
-#     print('Getting Results')
-#     totallist = []
-#     soup = BeautifulSoup(driver.page_source, "lxml")
-
-#     hclass = soup.find_all("div", {"class": "h-content"})
-
-#     for tag1 in hclass:
-#         htag = tag1.find_all("p", {"class": "name"})
-#         atag = tag1.find_all("p", {"class": "address"})
-#         ptag = tag1.find_all("div", {"class": "price"})
-#         dtag = tag1.find_all("", {"class": "mi"})
-#         for hotel, addy, price, mi in zip(htag, atag, ptag, dtag):
-#             print(hotel.contents[0])
-#             print(addy.get_text().strip())
-#             # print(price.get_text().strip())
-#             temp_price = re.findall('[0-9]{1,10}', price.get_text())
-#             print(int(temp_price[0]))
-
-#             temp_mi = re.findall('[0-9]{1,10}', mi.get_text())
-#             print(int(temp_mi[0]))
-
-#     with open("results.html", "w") as temp:
-#         temp.write(soup.prettify())
-#         driver.save_screenshot('screenshot.png')
-
-# elif "TERMS OF SERVICE" in html:
-#     print('On main page')
-# elif "Sorry..." in html:
-#     print('Bad page')
-# else:
-#     print("Don't know")
-#     with open("temp.html", "w") as temp:
-#         temp.write(driver.page_source)
-
-# driver.quit()
