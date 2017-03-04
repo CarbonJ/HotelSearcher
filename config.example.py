c01d2448ba0a4f822d1fcab4e8973c5b86de52c7
CarbonJ <justin.hanssen@gmail.com>
2017-03-03 22:35:59 -0500

    Updated config file

--- config.example.py
+++ config.example.py
@@ -2,3 +2,4 @@ url = "https://aws.passkey.com/reg/<HOUSING KEY>/"
 testchannel = "<Testing Slack Channel>"
 livechannel = "<Live Slack Channel>"
 slacktoken = "<SlackToken>"
+env = 'Test' # Test, Live, or Other
